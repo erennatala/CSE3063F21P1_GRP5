516827c562bad28165461e91927b7111c532bb5a
Ahsen <ahsenyagmur13@gmail.com>
2022-01-09 17:55:34 +0300

    Approver class created.

---
+++ Python_Project/src/python_project/Approver.py
@@ -0,0 +1,16 @@
+class Approver:
+
+    def __init__(self, student):
+        self.student = student
+
+    def capacity_checker(self, course):
+        student_list = list()
+        #  capacity = course.capacity
+        capacity = 0
+
+        if capacity > len(student_list):
+            return True
+        else:
+            # error will be added.
+            return False
+
