0576907926a34ca622392021790140d6944d3313
Hakan Kenar <hakankenar@icloud.com>
2022-01-09 18:24:57 +0300

    new methods added to Approver class

--- Python_Project/src/python_project/Approver.py
+++ Python_Project/src/python_project/Approver.py
@@ -4,9 +4,9 @@ class Approver:
         self.student = student
 
     def capacity_checker(self, course):
-        student_list = list()
-        #  capacity = course.capacity
-        capacity = 0
+        student_list = course.students
+        capacity = course.capacity
+        # capacity = 0
 
         if capacity > len(student_list):
             return True
@@ -14,3 +14,24 @@ class Approver:
             # error will be added.
             return False
 
+    def prerequisite_checker(self, course):
+        past_courses_list = self.student.past_courses
+        prerequisites_courses_list = course.prerequisites
+
+        for required in prerequisites_courses_list:
+            if required not in past_courses_list:
+                # error will be added
+                pass
+                if course not in self.student.non_taken_courses:
+                    self.student.non_taken_courses.append(course)
+
+    def is_elective_taken(self, course):
+        past_courses_list = self.student.past_courses
+        course_basket_list = self.student.course_basket
+
+        if course in past_courses_list:  # if and elif can be merged
+            return False
+        elif course in course_basket_list:
+            return False
+        else:
+            return True
