530f8875e70df69ed31a7b36b2865889d9af0dcc
Kerem Mican <47419589+Kenreks@users.noreply.github.com>
2022-01-10 00:59:46 +0300

    read and write infrastructure done

---
+++ Python_Project/src/python_project/InputReader.py
@@ -0,0 +1,70 @@
+import json
+import pathlib
+import os
+
+from Instructor import Instructor
+
+class InputReader:
+
+    def read_instructor(self):
+        path = pathlib.Path(__file__).parent.parent.joinpath('resources/instructors.json')
+        f = open(path, 'r', encoding='utf-8')
+        data = json.load(f)
+        for i in data:
+            id = i['instructorID']
+            name = i['name']
+            surname = i['surname']
+            fullname = i['fullName']
+            #Instructor(id, name, surname, fullname)
+
+        f.close()
+
+    def read_courses(self):
+        path = pathlib.Path(__file__).parent.parent.joinpath('resources/curriculum.json')
+        f = open(path, 'r', encoding='utf-8')
+        data = json.load(f)
+        for i in data:
+            for title_key, title_value in i.items():
+                for courseiter in title_value:
+
+                    course_id = courseiter['courseId']
+                    course_name = courseiter['courseName']
+                    type = courseiter['Type']
+                    credit = courseiter['Credit']
+                    ects = courseiter['ECTS']
+                    instructor = courseiter['Instructor']
+                    capacity = courseiter['Capacity']
+                    schedule = courseiter['Schedule']
+
+                    if title_key[0:8] == 'Semester': #semester mandatory dersler
+                        print("Mandatory course")
+                        # Course(courseId, courseName, capacity, credit, ects, instructor)
+
+                    elif title_key[1:3] == 'TE': #technical electives
+                        print("Technical Elective")
+
+                    elif title_key[1:8] == 'ENG-FTE': #eng-fte's
+                        print("Faculty Elective")
+
+                    elif title_key[1:13] == 'NTE / ENG-UE': #ntes
+                        print("Non-Technical Elective")
+
+        f.close()
+
+    def read_student(self):
+        path = pathlib.Path(__file__).parent.parent.joinpath('resources/students.json')
+        f = open(path, 'r', encoding='utf-8')
+        data = json.load(f)
+
+        for i in data:
+            index = i['index']
+            name = i['name']
+            surname = i['surname']
+            id = 1000+index
+            #Student(name, surname, id)
+
+    def read_transcript(self):
+        path = pathlib.Path(__file__).parent.parent.joinpath('resources/students.json')
+        f = open(path, 'r', encoding='utf-8')
+        data = json.load(f)
+
--- Python_Project/src/python_project/Instructor.py
+++ Python_Project/src/python_project/Instructor.py
@@ -1,2 +1,7 @@
 class Instructor:
-    pass
+
+    def __init__(self, id, name, surname, fullname):
+        self.id = id
+        self.name = name
+        self.surname = surname
+        self.fullname = fullname
