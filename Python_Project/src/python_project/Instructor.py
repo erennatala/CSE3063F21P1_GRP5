b8a6249aa2610352419690e5ed96d1e532d224f0
farzel <51860809+farzel@users.noreply.github.com>
2022-01-13 22:42:18 +0300

    Methods of Instructor class has been implemented.

--- Python_Project/src/python_project/Instructor.py
+++ Python_Project/src/python_project/Instructor.py
@@ -4,73 +4,117 @@ from Error import Error
 
 class Instructor:
 
-    course_basket = list()
-
     def __init__(self, id, name, surname):
         self.id = id
         self.name = name
         self.surname = surname
         self.fullname = name + ' ' + surname
 
-    def __iter__(self):
-        return self
+    def check_two_technical(self, student):
+        """
+           course variable gets in to course_basket via for loop
+           new_course_basket list gets created for courses which will not be deleted
+           courses_count is a count check for number of taken TE courses
+           first if statement checks for the course type
+           second if statement checks for the courses_count ,creates the error and adds course to the student's non_taken_courses
+        """
+        course_basket = student.course_basket
+        for course in course_basket:
 
-    # def __next__(self):
-    #
-    #     basket_size = len(self.course_basket)
-    #
-    #     if self.course_basket[] ==
+            new_course_basket = list()
+            courses_count = 0
 
-    def check_two_technical(self):
+            if course.course_type == 'TE':
+                courses_count += 1
 
-        #course_basket = student.course_basket
-        math103 = Course('math103', 'mat')
-        uni103 = Course('uni103', 'mat')
-        engr101 = Course('engr101', 'emgr')
-        phys104 = Course('phys104', 'fizk')
-        course_basket = [math103, engr101, uni103, phys104]
-        count = 0
-        course_iter = iter(course_basket)
-        new_course_basket = list()
-        while True:
-            try:
-                item = next(course_iter)
-
-                if isinstance(item, Course): #burası technical olcal
-                    count+=1
-
-                    if count>2:
-                        technicalElevtiveError = Error('advisor', item.course_id, 'twoTechnicalElective')
-                        technicalElevtiveError.raise_error()
-
-                        count -= 1
-                    else:
-                        new_course_basket.append(item)
+                if courses_count > 2:
+                    err = Error('Advisor', course, "Two Technical Elective")
+                    course.error_map["Two Technical Elective"].append(err)
+                    student.add_error(err)
+                    student.add_non_taken(course)
+                    courses_count -= 1
                 else:
-                    new_course_basket.append(item)
-            except StopIteration:
-                break
+                    new_course_basket.append(course)
+            else:
+                new_course_basket.append(course)
+
 
     def check_fte(self, student):
-        # course_basket = student.course_basket
-        pass
+        """
+           filtered variable filters the FTE courses in course_basket
+           try except statement makes the error creation
+           try -> looks for list(filtered)[0] and creates the error and adds course to the student's non_taken_courses
+           except -> will be filled in terms of IndexError
+        """
+        course_basket = student.course_basket
+        filtered = filter(lambda course: course.course_type == 'FTE', course_basket)
+
+        #instead of using for we can use try except
+        try:
+            course = list(filtered)[0]
+            err = Error('Advisor', course, "Graduation Year")
+            course.error_map["Graduation Year"].append(err)
+            student.add_error(err)
+            student.add_non_taken(course)
+
+        except IndexError:
+            pass
 
 
     def check_graduation_project(self, student):
-        pass
+        """
+           filtered variable filters the cse4197 course_id
+           if statement checks for students completed credits and if it is smaller than 165 it gets in to try except statement
+           try -> looks for list(filtered)[0] and creates the error and adds course to the student's non_taken_courses
+           except -> will be filled in terms of IndexError
+        """
+        course_basket = student.course_basket
+        filtered = filter(lambda course: course.course_id == "CSE4197", course_basket)
+        if student.completed_credit < 165:
+            try:
+                course = list(filtered)[0]
+                err = Error('Advisor', course, 'Project')
+                course.error_map['Project'].append(err)
+                student.add_error(err)
+                student.add_non_taken(course)
+
+            except IndexError:
+                pass
+
 
     def check_minimum_credit(self, student):
-        pass
+        """
+           course variable gets in to course_basket via for loop
+           if statement checks if course_type equals to TE and completed_credit < minimum_credit
+           creates the error and adds course to the student's non_taken_courses
+        """
+        course_basket = student.course_basket
+        for course in course_basket:
+            if course.course_type == 'TE' and student.completed_credit < course.mimimum_credit:
+                err = Error('Advisor', course, "Uncompleted Credit")
+                course.error_map["Uncompleted Credit"].append(err)
+                student.add_error(err)
+                student.add_non_taken(course)
 
-    def select_non_taken_course(self, course):
-        pass
 
     def check_collision(self, course_basket, student):
         pass
 
     def approve_student_basket(self, student):
-        pass
+        """first if statement checks for the season, if it is fall it calls the check_two_technical method
+           second if statment checks for the semester_id equals 7 or 8, if it is not equal them and season is Fall, check_fte method gets called
+           """
+        season = student.semester.season
+        semester_id = student.semester.semester_id
 
-inst = Instructor(1, 'ahmet', 'mehmet')
+        if season == 'Fall':
+            self.check_two_technical(student)
 
-inst.check_two_technical()
+        if not (semester_id == 7 or semester_id == 8) and season == 'Fall':
+            self.check_fte(student)
+
+        self.check_graduation_project(student)
+        self.check_minimum_credit(student)
+
+        course_basket = student.course_basket
+        self.check_collision(course_basket, student)
